7239db1af6c642539e337d56d58d35aa462ea96b
Rahul Thakor <rahthakor@gmail.com>
2015-06-20 23:21:55 +0530

    Adding Blank Forms by RahThakor

---
+++ PeculiarSofts/PeculiarTuitionERP/Transaction Module/frmPaymentSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PeculiarTuitionERP.Transaction_Module
+{
+    public partial class frmPaymentSetting : Form
+    {
+        public frmPaymentSetting()
+        {
+            InitializeComponent();
+        }
+    }
+}

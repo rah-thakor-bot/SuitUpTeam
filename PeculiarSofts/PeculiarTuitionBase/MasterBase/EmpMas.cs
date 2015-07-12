57e940c19b5c84d8c7e95815002f3f8c99e14468
Rahul Thakor <rahthakor@gmail.com>
2015-07-12 23:56:16 +0530

    Pending Files

---
+++ PeculiarSofts/PeculiarTuitionBase/MasterBase/EmpMas.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections;
+
+namespace PeculiarTuitionBase.MasterBase
+{
+    public class EmpMas : TuitionBase
+    {
+        public Hashtable Savedata(string branch,char flg, string empType, string fName, string mName, string lName, DateTime dob , DateTime doj, string sex,string bldGrp, string ph1, string ph2, string adr1, string adr2, string city, string state, string pincode, string email,string user,string terminal, out string errMsg)
+        {
+            errMsg = string.Empty;
+            Hashtable ht = new Hashtable();
+            try
+            {
+                _base.Connect();
+                if (flg == 'I')
+                {
+                    _base.AddInParam("p_branch", DbType.String, branch);
+                    _base.AddInParam("p_f_name", DbType.String, fName);
+                    _base.AddInParam("p_m_name", DbType.String, mName);
+                    _base.AddInParam("p_l_name", DbType.String, lName);
+                    _base.AddInParam("p_dob", DbType.Date, null);
+                    _base.AddInParam("p_doj", DbType.Date, null);
+                    _base.AddInParam("p_leave_date", DbType.Date, null);
+                    _base.AddInParam("p_sex", DbType.String, sex);
+                    _base.AddInParam("p_blood_grp", DbType.String, bldGrp);
+                    _base.AddInParam("p_phone1", DbType.Int64, ph1);
+                    _base.AddInParam("p_phone2", DbType.Int64, sex);
+                    _base.AddInParam("p_photo", DbType.Object, null);
+                    _base.AddInParam("p_add1", DbType.String, sex);
+                    _base.AddInParam("p_add2", DbType.String, sex);
+                    _base.AddInParam("p_city", DbType.String, sex);
+                    _base.AddInParam("p_state", DbType.String, sex);
+                    _base.AddInParam("p_pincode", DbType.String, sex);
+                    _base.AddInParam("p_email_id", DbType.String, sex);
+                    _base.AddInParam("p_ent_user", DbType.String, sex);
+                    _base.AddInParam("p_ent_term", DbType.String, sex);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                errMsg = ex.ToString();
+            }
+            finally
+            {
+
+            }
+            return ht;
+        }
+    }
+}
+/*  
+
+p_dob date    in null
+p_doj date    in null
+p_leave_date date    in null
+p_sex varchar2(1) in null
+p_blood_grp varchar2(10)    in null
+p_phone1 number  in	1
+p_phone2 number  in	1
+p_photo clob    in null
+p_add1 varchar2(100)   in null
+p_add2 varchar2(100)   in null
+p_city varchar2(20)    in null
+p_state varchar2(20)    in null
+p_pincode number  in	1
+p_email_id varchar2(50)    in null
+p_ent_user varchar2(30)    in null
+p_ent_term varchar2(30)    in null
+p_time_stamp timestamp   in/out null
+p_entity_id number  out n/a
+p_flg   varchar2(200)   out n/a
+p_msg   varchar2(200)   out n/a
+*/
---
+++ PeculiarSofts/PeculiarTuitionERP/Master Module/frmEmpMas.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using PeculiarTuitionBase.MasterBase;
+using System.Globalization;
+
+
+
+namespace PeculiarTuitionERP.Master_Module
+{
+    public partial class frmEmpMas : frmBaseChild
+    {
+        private string CurrentAction = string.Empty;
+        private char action_flg = 'I';
+        private EmpMas libEmpMas;
+        private string errMsg = string.Empty;
+        string branch, emp_type, fname, mname, lname, sex, bldGrp, ph1, ph2, adr1, adr2, city, state, pincode, email_id;
+        DateTime dob, doj;
+        CultureInfo DatetimeCulture;
+        Hashtable GetNullParamters;
+
+
+        public frmEmpMas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEmpMas_Load(object sender, EventArgs e)
+        {
+            libEmpMas = new EmpMas();
+            SetDefaultState();
+        }
+
+
+        public void SetDefaultState()
+        {
+            btnEdit.Enabled = btnDelete.Enabled = btnRefresh.Enabled = false;
+            branch = emp_type = fname = mname = lname = sex = bldGrp = ph1 = ph2 = adr1 = adr2 = city = state = pincode = email_id = string.Empty;
+            dob = doj = DateTime.Now;
+            DatetimeCulture = new CultureInfo("en-US", false);
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (btnAdd.Text == "&Add")
+            {
+                CurrentAction = "ADD";
+                btnAdd.Text = "&Save";
+                btnEdit.Text = "&Cancel";
+                btnDelete.Enabled = true;
+                btnSearch.Enabled = btnRefresh.Enabled = false;
+            }
+            else//btnAdd Text = Save
+            {
+                Hashtable saveSummary = Savedata();    
+            }
+        }
+
+        private Hashtable Savedata()
+        {
+            Hashtable SaveResponse = new Hashtable();
+            try
+            {
+                GetCurrentValues();
+                if (GetNullParamters.Keys.Count == 0)
+                {
+                    if (libEmpMas == null)
+                        libEmpMas = new EmpMas();
+                    SaveResponse = libEmpMas.Savedata(branch, action_flg, emp_type, fname, mname, lname, dob, doj, sex, bldGrp, ph1, ph2, adr1, adr2, city, state, pincode, email_id, null, Environment.MachineName.ToString(), out errMsg);
+                    if (!string.IsNullOrEmpty(errMsg)) throw new Exception(errMsg);
+                    if (SaveResponse.Contains("p_flg"))
+                    {
+                        if (SaveResponse["p_flg"].ToString() == "Y")
+                        {
+
+                        }
+                        else //Problem handle with error msg from back end
+                        {
+
+                        }
+
+                    }
+                }
+                else// Some Null Value are there
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            } 
+           
+            return SaveResponse;
+        }
+
+        private void GetCurrentValues()
+        {
+            GetNullParamters = new Hashtable();
+
+            {//Emp Type
+                if (rdbtnStd.Checked)
+                    emp_type = "Student";
+                else if (rdbtnTeacher.Checked)
+                    emp_type = "Teacher";
+                else
+                    emp_type = "Other";
+            }
+
+            {//First Name
+                if (!string.IsNullOrEmpty(txtbxFname.Text))
+                    fname = txtbxFname.Text;
+                else
+                    GetNullParamters.Add(txtbxFname, String.Empty);
+            }
+
+            {//Middle Name
+                if (!string.IsNullOrEmpty(txtbxMname.Text))
+                    mname = txtbxMname.Text;
+                else
+                    GetNullParamters.Add(txtbxMname, String.Empty);
+            }
+
+            {//Last Name
+                if (!string.IsNullOrEmpty(txtbxLname.Text))
+                    lname = txtbxLname.Text;
+                else
+                    GetNullParamters.Add(txtbxLname, String.Empty);
+            }
+
+            //Peding Code for Datetime format
+
+            {//Sex 
+                if (!string.IsNullOrEmpty(cmbSex.Text))
+                    sex = cmbSex.Text;
+                else
+                    GetNullParamters.Add(cmbSex, String.Empty);
+            }
+            {//Primary Phone
+                if (!string.IsNullOrEmpty(txtbxPh1.Text))
+                    ph1 = txtbxPh1.Text;
+                else
+                    GetNullParamters.Add(txtbxPh1, String.Empty);
+            }
+            {//Secondary Phone
+                if (!string.IsNullOrEmpty(txtbxPh2.Text))
+                    ph2 = txtbxPh2.Text;
+                else
+                    GetNullParamters.Add(txtbxPh2, String.Empty);
+            }
+            {
+                if (!string.IsNullOrEmpty(txtbxBldGrp.Text))
+                    bldGrp = txtbxBldGrp.Text;
+                else
+                    GetNullParamters.Add(txtbxBldGrp, String.Empty);
+            }
+            {//Address 1
+                if (!string.IsNullOrEmpty(txtbxAdr1.Text))
+                    adr1 = txtbxAdr1.Text;
+                else
+                    GetNullParamters.Add(txtbxAdr1, String.Empty);
+            }
+            {//Address 2
+                if (!string.IsNullOrEmpty(txtbxAdr2.Text))
+                    adr2 = txtbxAdr2.Text;
+                else
+                    GetNullParamters.Add(txtbxAdr2, String.Empty);
+            }
+            {//City
+                if (!string.IsNullOrEmpty(txtbxCity.Text))
+                    city = txtbxCity.Text;
+                else
+                    GetNullParamters.Add(txtbxCity, String.Empty);
+            }
+            {//State
+                if (!string.IsNullOrEmpty(txtbxState.Text))
+                    state = txtbxState.Text;
+                else
+                    GetNullParamters.Add(txtbxState, String.Empty);
+            }
+            {//Pincode
+                if (!string.IsNullOrEmpty(txtbxPincode.Text))
+                    pincode = txtbxPincode.Text;
+                else
+                    GetNullParamters.Add(txtbxPincode, String.Empty);
+            }
+            {//Email
+                if (!string.IsNullOrEmpty(txtbxEmail.Text))
+                    email_id = txtbxEmail.Text;
+                else
+                    GetNullParamters.Add(txtbxEmail, String.Empty);
+            }
+        }
+    }
+}
